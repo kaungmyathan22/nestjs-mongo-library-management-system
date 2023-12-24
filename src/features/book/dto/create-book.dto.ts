a82098834e29bfa9cace93c8295e0f252aa70e19
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-24 15:08:38 +0630

    feat(book): create book dto added

--- src/features/book/dto/create-book.dto.ts
+++ src/features/book/dto/create-book.dto.ts
@@ -1,1 +1,13 @@
-export class CreateBookDto {}
+import { IsMongoId, IsNumber, IsString, Min } from 'class-validator';
+
+export class CreateBookDto {
+  @IsString()
+  name: string;
+  @IsString()
+  description: string;
+  @IsMongoId()
+  authorId: string;
+  @IsNumber()
+  @Min(1)
+  stockCount: number;
+}
