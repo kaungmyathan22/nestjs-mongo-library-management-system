3a46a0ea22621306eb5994e5e0e3f34dc8bc5d27
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-24 15:07:42 +0630

    update(): book repository / book model added

--- src/features/book/book.module.ts
+++ src/features/book/book.module.ts
@@ -3,7 +3,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthorController } from './controllers/author.controller';
 import { BookController } from './controllers/book.controller';
 import { AuthorRepository } from './repositories/author.repository';
+import { BookRepository } from './repositories/book.repository';
 import { Author, AuthorSchema } from './schemas/author.schema';
+import { Book, BookSchema } from './schemas/book.schema';
 import { AuthorService } from './services/author.service';
 import { BookService } from './services/book.service';
 
@@ -14,9 +16,13 @@ import { BookService } from './services/book.service';
         name: Author.name,
         schema: AuthorSchema,
       },
+      {
+        name: Book.name,
+        schema: BookSchema,
+      },
     ]),
   ],
   controllers: [BookController, AuthorController],
-  providers: [BookService, AuthorService, AuthorRepository],
+  providers: [BookService, AuthorService, AuthorRepository, BookRepository],
 })
 export class BookModule {}
