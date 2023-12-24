e47ba6dc002acfbfb201b0ae928b1b80b4469391
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-24 15:08:53 +0630

    feat(book): controller type adjsuted

--- src/features/book/controllers/book.controller.ts
+++ src/features/book/controllers/book.controller.ts
@@ -3,15 +3,18 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { CreateBookDto } from '../dto/create-book.dto';
 import { UpdateBookDto } from '../dto/update-book.dto';
 import { BookService } from '../services/book.service';
 
-@Controller('book')
+@Controller('api/v1/books')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
@@ -21,22 +24,23 @@ export class BookController {
   }
 
   @Get()
-  findAll() {
-    return this.bookService.findAll();
+  findAll(@Query() queryParams) {
+    return this.bookService.findAll(queryParams);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.bookService.findOne(+id);
+    return this.bookService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
-    return this.bookService.update(+id, updateBookDto);
+    return this.bookService.update(id, updateBookDto);
   }
 
+  @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.bookService.remove(+id);
+    return this.bookService.remove(id);
   }
 }
