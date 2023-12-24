a0332360049e00a46256bea0ce7c8dc73665e075
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-24 15:09:19 +0630

    feat(book): book crud service added

--- src/features/book/services/book.service.ts
+++ src/features/book/services/book.service.ts
@@ -1,26 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import mongoose from 'mongoose';
+import { EnvironmentConstants } from 'src/common/constants/environment.constants';
+import { PaginatedParamsDto } from 'src/common/dto/paginated-query.dto';
 import { CreateBookDto } from '../dto/create-book.dto';
 import { UpdateBookDto } from '../dto/update-book.dto';
+import { AuthorRepository } from '../repositories/author.repository';
+import { BookRepository } from '../repositories/book.repository';
 
 @Injectable()
 export class BookService {
-  create(createBookDto: CreateBookDto) {
-    return 'This action adds a new book';
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly bookRepository: BookRepository,
+    private readonly authorRepository: AuthorRepository,
+  ) {}
+
+  async create(payload: CreateBookDto) {
+    try {
+      const author = await this.authorRepository.findOne(
+        new mongoose.Types.ObjectId(payload.authorId),
+        'Author with given id not found.',
+      );
+      return await this.bookRepository.create({
+        ...payload,
+        author: author._id,
+      } as any);
+    } catch (error) {
+      if (
+        error.code ===
+        +this.configService.get(EnvironmentConstants.DUPLICATE_ERROR_KEY)
+      ) {
+        throw new ConflictException(
+          `Book with name(${payload.name}) already exists.`,
+        );
+      }
+      throw error;
+    }
   }
 
-  findAll() {
-    return `This action returns all book`;
+  findAll(queryParams: PaginatedParamsDto) {
+    return this.bookRepository.findAllWithPaginated(queryParams);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} book`;
+  async findOne(id: string) {
+    const book = await this.bookRepository.findOne(
+      {
+        _id: id,
+      },
+      'Book with given id not found.',
+    );
+    return book;
   }
 
-  update(id: number, updateBookDto: UpdateBookDto) {
-    return `This action updates a #${id} book`;
+  async update(id: string, updateBookDto: UpdateBookDto) {
+    const product = await this.findOne(id);
+    return this.bookRepository.findOneAndUpdate(
+      { id: product.id },
+      { ...updateBookDto },
+    );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} book`;
+  async remove(id: string) {
+    const book = await this.findOne(id);
+    await this.bookRepository.remove({ _id: book._id });
+    return { success: true };
   }
 }
