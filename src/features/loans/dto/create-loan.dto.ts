3883579d442d51bd7d4303fb4595ddace63eb77f
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-24 20:22:49 +0630

    feat(book-loan): create loan dto added

--- src/features/loans/dto/create-loan.dto.ts
+++ src/features/loans/dto/create-loan.dto.ts
@@ -1,1 +1,17 @@
-export class CreateLoanDto {}
+import { IsEnum, IsISO8601, IsMongoId } from 'class-validator';
+
+export enum StatusEnum {
+  RETURNED = 'RETURNED',
+  LENT = 'LENT',
+}
+
+export class CreateLoanDto {
+  @IsMongoId()
+  borrowerId: string;
+  @IsMongoId()
+  bookId: string;
+  @IsEnum(StatusEnum, { message: 'Invalid status' })
+  status: StatusEnum;
+  @IsISO8601({ strict: true }, { message: 'Invalid ISO 8601 date format' })
+  startDate: Date;
+}
